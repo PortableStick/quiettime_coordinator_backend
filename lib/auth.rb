12eaa0a8d37447899d92ba34b85cba2f576c6268
Gregory Smith <crashingwaves@fea.st>
2016-11-30 17:30:30 -0700

    Updated

--- lib/auth.rb
+++ lib/auth.rb
@@ -3,11 +3,11 @@ require 'jwt'
 class Auth
   ALGORITHM = 'HS256'.freeze
 
-  def issue(payload)
+  def self.issue(payload)
     JWT.encode(payload, Rails.application.secrets.secret_key_base, ALGORITHM)
   end
 
-  def decode(token)
+  def self.decode(token)
     JWT.decode(token, Rails.application.secrets.secret_key_base, true, algorithm: ALGORITHM).first
   end
 end
--- spec/support/sessions_spec_helper.rb
+++
@@ -1,7 +0,0 @@
-module SessionsHelper
-  module Requests
-    def post_request
-      post :create, auth: { email: user.email, password: user.password }
-    end
-  end
-end
