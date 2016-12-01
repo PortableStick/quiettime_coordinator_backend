ee808cd21b3f7833c05d8c622faa7f7280f4ff20
Gregory Smith <crashingwaves@fea.st>
2016-12-01 12:08:38 -0700

    Fix broken controller

--- app/controllers/api/v1/tokens_controller.rb
+++ app/controllers/api/v1/tokens_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::TokensController < ApplicationController
   def create
-    user = User.find_by(email: user_params["email"])
-    return render json: { jwt: Auth.issue(user: user.id) } if user && user.authenticate(user_params["password"])
+    user = User.find_by(email: user_params[:email])
+    return render json: { jwt: Auth.issue(user: user.id) } if user && user.authenticate(user_params[:password])
     render json: { message: "Invalid credentials" }, status: 401
   end
 
