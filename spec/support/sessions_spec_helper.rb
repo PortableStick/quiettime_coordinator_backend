04f20e00cde47bd6b3814087a694eab2ad8d47d0
Gregory Smith <crashingwaves@fea.st>
2016-11-30 19:10:10 -0700

    Delete the cruft

--- spec/support/sessions_spec_helper.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-module SessionsHelper
-  module Requests
-    def self.post_request(user)
-      post api_v1_login_path, auth: { email: user.email, password: user.password }
-    end
-  end
-end
