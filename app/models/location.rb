717f8ba6e8a336296cec69175855b6e25873d008
Gregory Smith <crashingwaves@fea.st>
2016-11-30 19:05:13 -0700

    Add methods to change location attendence

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,11 @@
 class Location < ApplicationRecord
+  def increment_attendence
+    increment(:attending, 1)
+    save
+  end
+
+  def decrement_attendence
+    decrement(:attending, 1)
+    save
+  end
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Location, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:loc) { create(:location) }
+
+  describe "#increment_attendence" do
+    it 'should increment attendence count by 1' do
+      expect{ loc.increment_attendence }.to change{ loc.attending }.by(1)
+    end
+
+    it 'should save the incremented attendence' do
+      expect{ loc.increment_attendence }.to change{ loc.attending }.by(1)
+      new_attending = Location.find_by(coords: loc.coords).attending
+      expect(new_attending).to eq(loc.attending)
+    end
+  end
+
+  describe '#decrement_attendence' do
+    it 'should decrement attendence count by 1' do
+      expect{ loc.decrement_attendence }.to change{ loc.attending }.by(-1)
+    end
+
+    it 'should save the decremented attendence' do
+      expect{ loc.decrement_attendence }.to change{ loc.attending }.by(-1)
+      new_attending = Location.find_by(coords: loc.coords).attending
+      expect(new_attending).to eq(loc.attending)
+    end
+  end
 end
