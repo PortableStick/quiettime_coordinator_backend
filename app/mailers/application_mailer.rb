5540fe29b4906fc2545530c1082c4c7a17ed0683
Gregory Smith <crashingwaves@fea.st>
2016-12-06 22:31:57 -0700

    Add notifier

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'support@quiettimecoordinator.com'
   layout 'mailer'
 end
---
+++ app/mailers/notifier_mailer.rb
@@ -0,0 +1,9 @@
+class NotifierMailer < ApplicationMailer
+  default_url_options[:host] = "localhost:3000"
+
+  def password_reset(user)
+    @user = user
+    mail(to: "#{user[:username]} <#{user[:email]}>",
+      subject: "Reset your password")
+  end
+end
